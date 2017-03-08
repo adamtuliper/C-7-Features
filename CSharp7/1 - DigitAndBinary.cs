0f5bfbba63f4f6a1e1f03f2c4b69f0b8c7f686e6
Adam Tuliper <adamt@microsoft.com>
2017-03-08 10:55:55 -0800

    Touchups

--- CSharp7/1 - DigitAndBinary.cs
+++ CSharp7/1 - DigitAndBinary.cs
@@ -56,9 +56,9 @@ namespace c_sharp_7.CSharp7
             var sb = new StringBuilder();
 
             //separator can be used in various places.
-            long age_of_universe = 13_082_000_000;
+            long age_of_universe = 13_082_000_001;
             Console.WriteLine(age_of_universe);
-
+            
             //Binary literal (with separator)
             Int32 bytes1 = 0b01001000_01100101_01101100_01101100;
             Int32 bytes2 = 0b01101111_00100000_01010111_01101111;
