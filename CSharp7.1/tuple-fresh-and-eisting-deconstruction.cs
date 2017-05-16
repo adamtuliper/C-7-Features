af63cc417cf8baf884f82148e6f09510020f7914
Adam Tuliper <adam.tuliper@gmail.com>
2017-05-16 00:09:22 -0700

    minor

--- CSharp7.1/tuple-fresh-and-eisting-deconstruction.cs
+++ CSharp7.1/tuple-fresh-and-eisting-deconstruction.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace c_sharp_7.CSharp7._1
 {
-    class tuple_mixing_fresh_and_exsting_deconstruction
+    class tuple_mixing_fresh_and_existing_deconstruction
     {
         public void Run()
         {
